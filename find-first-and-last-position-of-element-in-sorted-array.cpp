d86de7fc964d2d8abc2ad804d6dacfc92ecc3a61
adilmp1 <115640364+adilmp1@users.noreply.github.com>
2023-10-09 19:33:27 +0530

    Update find-first-and-last-position-of-element-in-sorted-array.cpp

--- find-first-and-last-position-of-element-in-sorted-array.cpp
+++ find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,3 +1,4 @@
+// solution 1
 int binarysearch(vector<int> a, int low, int high, int k)
 {
     int f = low;
@@ -46,4 +47,80 @@ vector<int> searchRange(vector<int> a, int k)
     }
     ans.push_back(right);
     return ans;
+}
+
+
+// solution 2
+int leftBinarySearch(vector<int> a, int low, int high, int k)
+{
+    int f = low;
+    int l = high;
+    int m;
+    while (f <= l)
+    {
+        m = (f + l) / 2;
+        if (a[m] == k)
+        {
+            int x = leftBinarySearch(a,0,m-1,k);
+            if(x!=-1)
+            {
+                return min(m,x);
+            }
+            else
+            {
+                return m;
+            }
+            
+        }
+        else if (a[m] > k)
+        {
+            l = m - 1;
+        }
+        else
+        {
+            f = m + 1;
+        }
+    }
+    return -1;
+}
+int rightBinarySearch(vector<int> a, int low, int high, int k)
+{
+    int f = low;
+    int l = high;
+    int m;
+    while (f <= l)
+    {
+        m = (f + l) / 2;
+        if (a[m] == k)
+        {
+            int x = rightBinarySearch(a,m+1,a.size()-1,k);
+            if(x!=-1)
+            {
+                return max(m,x);
+            }
+            else
+            {
+                return m;
+            }
+            
+        }
+        else if (a[m] > k)
+        {
+            l = m - 1;
+        }
+        else
+        {
+            f = m + 1;
+        }
+    }
+    return -1;
+}
+vector<int> searchRange(vector<int> a, int k)
+{
+    vector<int> ans;
+    int n = a.size();
+    int i = n - 1;
+    int left = leftBinarySearch(a,0,n-1,k);
+    int right = rightBinarySearch(a,0,n-1,k);
+    return {left,right};
 }
