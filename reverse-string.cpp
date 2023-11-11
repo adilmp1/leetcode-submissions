30d70713d0a562a47df7c2a82301c34b788a25df
adilmp1 <115640364+adilmp1@users.noreply.github.com>
2023-11-11 10:29:45 +0530

    Create reverse-string.cpp

---
+++ reverse-string.cpp
@@ -0,0 +1,12 @@
+    void helper(int i,int j,vector<char>& s)
+    {
+        if(i>=j)
+        {
+            return;
+        }
+        swap(s[i],s[j]);
+        helper(i+1,j-1,s);
+    }
+    void reverseString(vector<char>& s) {
+        helper(0,s.size()-1,s);
+    }
