4ee5b99ee2e9d74476606acfab4c53cd348fc91d
adilmp1 <115640364+adilmp1@users.noreply.github.com>
2024-02-21 18:13:15 +0530

    Create assign-cookies.cpp

---
+++ assign-cookies.cpp
@@ -0,0 +1,21 @@
+    int findContentChildren(vector<int>& g, vector<int>& s) 
+    {
+        int ans=0;
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int i=0;
+        int j=0;
+        while(i<g.size() and j<s.size())
+        {
+            if(g[i]<=s[j])
+            {
+                ans++;
+                i++;j++;
+            }
+            else 
+            {
+                j++;
+            }
+        }
+        return ans;
+    }
