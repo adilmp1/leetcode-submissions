4a81d33af56f6237791a59d241758b4fe358c4f1
Adil Mohamed M P <adilmohamedmp1@gmail.com>
2024-06-28 11:26:08 +0530

    Update partition-labels.cpp

--- partition-labels.cpp
+++ partition-labels.cpp
@@ -1,3 +1,4 @@
+// First Approach 
     vector<int> partitionLabels(string s) 
     {
         unordered_map<char,int> freq;
@@ -29,6 +30,31 @@
                 length=0;
                 mp.clear();
                 st.clear();
+            }
+        }
+        return ans;
+    }
+
+// Optimized
+    vector<int> partitionLabels(string s) 
+    {
+        unordered_map<char,int> last;
+        for(int i=0;i<s.size();i++)
+        {
+            last[s[i]]=i;
+        }
+        vector<int> ans;
+        int length=0;
+        int lastIndex=INT_MIN;
+        for(int i=0;i<s.size();i++)
+        {
+            lastIndex = max(last[s[i]],lastIndex);
+            length++;
+            if(i==lastIndex)
+            {
+                ans.push_back(length);
+                lastIndex=INT_MIN;
+                length=0;
             }
         }
         return ans;
