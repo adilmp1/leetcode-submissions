2299941d7d7aa431c38e76844d4dd356db744903
Adil Mohamed M P <adilmohamedmp1@gmail.com>
2024-07-20 19:44:53 +0530

    Create coin-change-ii.cpp

---
+++ coin-change-ii.cpp
@@ -0,0 +1,19 @@
+int changeHelper(int target,vector<int>& a,int idx,vector<vector<int>>& dp)
+{
+    if(target==0) return 1;
+    if(target<0) return 0;
+    if(idx>=a.size()) return 0;
+    if(dp[idx][target]!=-1) return dp[idx][target];
+    int ans=0;
+    for(int i=idx;i<a.size();i++)
+    {
+        ans+=changeHelper(target-a[i],a,i,dp);
+    }
+    
+    return dp[idx][target]=ans;
+}
+int change(int amount, vector<int>& a)
+{
+    vector<vector<int>> dp(a.size(),vector<int>(amount+1,-1));
+    return changeHelper(amount,a,0,dp);
+}
