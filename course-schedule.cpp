45363e616ac302c13880fb424be3693569004e06
Adil Mohamed M P <adilmohamedmp1@gmail.com>
2024-08-16 21:58:33 +0530

    Update course-schedule.cpp

--- course-schedule.cpp
+++ course-schedule.cpp
@@ -1,3 +1,48 @@
+// indegree method
+bool canFinish(int numCourses, vector<vector<int>> &a)
+{
+    unordered_map<int,vector<int>> mp;
+    unordered_map<int,bool> dp;
+    vector<int> indegree(numCourses,0);
+    for(auto i:a)
+    {
+        mp[i[1]].push_back(i[0]);
+        indegree[i[0]]++;
+    }
+    int completed=0;
+    queue<int> q;
+    for(int i=0;i<numCourses;i++)
+    {
+        if(!indegree[i])
+        {
+            q.push(i);
+            dp[i]=true;
+        }
+    }
+    while(!q.empty())
+    {
+        int idx=q.front();q.pop();
+        for(auto i:a)
+        {
+            if(i[1]==idx)
+            {
+                indegree[i[0]]--;
+            }
+        }
+        for(int i=0;i<numCourses;i++)
+        {
+            if(!indegree[i] and !dp[i])
+            {
+                dp[i]=true;
+                q.push(i);
+            }
+        }
+
+    }
+    return (dp.size()==numCourses);
+}
+
+// recursive dfs method
 bool dfs(int idx,vector<bool>& vis,unordered_map<int,vector<int>>& mp,vector<bool> dp)
 {
     if(vis[idx]) 
