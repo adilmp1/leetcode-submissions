b01165a04ce0793c7568aa7cc46418931e1e54ad
Adil Mohamed M P <adilmohamedmp1@gmail.com>
2024-09-11 07:24:27 +0530

    Update minimum-bit-flips-to-convert-number.cpp

--- minimum-bit-flips-to-convert-number.cpp
+++ minimum-bit-flips-to-convert-number.cpp
@@ -1,3 +1,16 @@
+// new code
+int minBitFlips(int a, int b)
+{
+    int count=0;
+    for(int i=0;i<32;i++)
+    {
+        count+=(a%2 != b%2);
+        a/=2;
+        b/=2;
+    }
+    return count;
+}
+
     int minBitFlips(int start, int goal) {
         int ans=0;
         while(start or goal)
