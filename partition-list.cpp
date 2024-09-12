7dc79aa2c865cfd41777678c8eeb393387ec04ec
Adil Mohamed M P <adilmohamedmp1@gmail.com>
2024-09-12 22:14:40 +0530

    Update partition-list.cpp

--- partition-list.cpp
+++ partition-list.cpp
@@ -1,3 +1,32 @@
+    // real solution
+    #define node ListNode
+    ListNode* partition(ListNode* head, int x) {
+        node * list1 = new node(0);
+        node * list2 = new node(0);
+
+        node * l1 = list1;
+        node * l2 = list2;
+
+        node * p =head;
+        while(p)
+        {
+            if(p->val<x)
+            {
+                l1->next=p;
+                l1=l1->next;
+            }
+            else
+            {
+                l2->next=p;
+                l2=l2->next;
+            }
+            p=p->next;
+        }
+        l1->next=list2->next;
+        l2->next=NULL;
+        return list1->next;
+    }
+
     // udayip solution
     #define node ListNode
     ListNode* partition(ListNode* head, int x) {
