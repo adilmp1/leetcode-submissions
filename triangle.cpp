e2c60d8f95fd927dfca95fdcfe331d8d302e804e
Adil Mohamed M P <adilmohamedmp1@gmail.com>
2025-01-16 19:48:03 +0530

    Update triangle.cpp

--- triangle.cpp
+++ triangle.cpp
@@ -1,3 +1,29 @@
+// bottom up dp
+
+int minimumTotal(vector<vector<int>>& a)
+{
+    int n = a.size();
+    vector<int> dp(n,0);
+    for(int i=0;i<n;i++)
+    {
+        dp[i]=a[n-1][i];
+    }
+    
+    for(int i=n-2;i>=0;i--)
+    {
+        vector<int> temp(n,0);
+        for(int j=i;j>=0;j--)
+        {
+            temp[j] = min(a[i][j]+dp[j],a[i][j]+dp[j+1]);
+        }
+        dp=temp;
+    }
+    return dp[0];
+}
+
+
+
+// recursion + memoization 
 int helper(int row,int start,vector<vector<int>>& a,vector<vector<int>>& dp)
 {
     if(row==a.size()) return 0;
