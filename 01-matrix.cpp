4f7367393ff30d023044ba0ca163fcf273b59829
Adil Mohamed M P <adilmohamedmp1@gmail.com>
2025-01-22 15:06:51 +0530

    Update 01-matrix.cpp

--- 01-matrix.cpp
+++ 01-matrix.cpp
@@ -1,50 +1,36 @@
-#define pip pair<int,pair<int, int>>
-
-bool isValid(int x,int y,int m,int n)
-{
-    return (x>=0 and y>=0 and x<m and y<n);
+bool isValid(int x, int y, int m, int n) {
+    return (x >= 0 && y >= 0 && x < m && y < n);
 }
 
-vector<vector<int>> updateMatrix(vector<vector<int>> &a)
-{
-    priority_queue<pip, vector<pip>, greater<pip>> pq;
+vector<vector<int>> updateMatrix(vector<vector<int>> &a) {
     int m = a.size();
     int n = a[0].size();
-    vector<vector<int>> ans(m,vector<int>(n,INT_MAX));
-    vector<vector<bool>> visited(m,vector<bool>(n,false));
-    for(int i=0;i<m;i++)
-    {
-        for(int j=0;j<n;j++)
-        {
-            if(!a[i][j])
-            {
-                ans[i][j]=0;
-                pq.push({0,{i,j}});
+    vector<vector<int>> ans(m, vector<int>(n, INT_MAX));
+    queue<pair<int, int>> q;
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            if (a[i][j] == 0) {
+                ans[i][j] = 0;
+                q.push({i, j});
             }
         }
     }
-    vector<int> X = {0,0,1,-1};
-    vector<int> Y = {1,-1,0,0};
-    while(!pq.empty())
-    {
-        int x = pq.top().second.first;
-        int y = pq.top().second.second;
-        pq.pop();
-        if(visited[x][y]) continue;;
-        visited[x][y]=true;
-        cout<<x<<" "<<y<<endl;  
-        for(int i=0;i<4;i++)
-        {
-            int row = x+X[i];
-            int col = y+Y[i];
-            if(isValid(row,col,m,n))
-            {
-                int cost = ans[x][y]+1;
-                if(cost<ans[row][col])
-                {
-                    ans[row][col] = cost;
-                    pq.push({cost,{row,col}});
-                }
+
+    vector<int> X = {0, 0, 1, -1};
+    vector<int> Y = {1, -1, 0, 0};
+
+    while (!q.empty()) {
+        int x = q.front().first;
+        int y = q.front().second;
+        q.pop();
+
+        for (int i = 0; i < 4; i++) {
+            int row = x + X[i];
+            int col = y + Y[i];
+            if (isValid(row, col, m, n) && ans[row][col] > ans[x][y] + 1) {
+                ans[row][col] = ans[x][y] + 1;
+                q.push({row, col});
             }
         }
     }
