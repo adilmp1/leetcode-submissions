2b851cabb213b751219b7f0952cfbff922ab767c
Adil Mohamed M P <adilmohamedmp1@gmail.com>
2025-01-25 20:24:59 +0530

    Create minimum-depth-of-binary-tree.cpp

---
+++ minimum-depth-of-binary-tree.cpp
@@ -0,0 +1,9 @@
+    int helper(TreeNode* root) {
+        if(!root) return INT_MAX;
+        if(!root->left and !root->right) return 1;
+        return 1+min(helper(root->left),helper(root->right));
+    }
+    int minDepth(TreeNode* root) {
+        if(!root) return 0;
+        return helper(root);
+    }
