95fa2faf419c8fa9d3b842ae86cdc9929596c8b4
Adil Mohamed M P <adilmohamedmp1@gmail.com>
2025-02-18 10:59:20 +0530

    Update remove-duplicates-from-sorted-array-ii.cpp

--- remove-duplicates-from-sorted-array-ii.cpp
+++ remove-duplicates-from-sorted-array-ii.cpp
@@ -1,20 +1,6 @@
     int removeDuplicates(vector<int>& a) 
     {
-        int n = a.size();
-        for(int i=0;i<n-2;i++)
-        {
-            if(a[i]==a[i+1] and a[i]==a[i+2])
-            {
-                a[i]=INT_MAX;
-            }
-        }
-        for(int i=0;i<n;i++)
-        {
-            if(a[i]==INT_MAX)
-            {
-                a.erase(a.begin()+i);
-                i--;
-            }
-        }
-        return a.size();
+        int idx=0;
+        for(int x:a) if(idx==0 or idx==1 or a[idx-2]!=x) a[idx++]=x;
+        return idx;
     }
