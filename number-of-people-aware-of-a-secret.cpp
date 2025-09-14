434e4a7f2f3a9c961f701eebffc43f33ef669c3f
Adil Mohamed M P <adilmohamedmp1@gmail.com>
2025-09-14 10:38:50 +0530

    Update number-of-people-aware-of-a-secret.cpp

--- number-of-people-aware-of-a-secret.cpp
+++ number-of-people-aware-of-a-secret.cpp
@@ -1,3 +1,25 @@
+// bottom up dp, tabulation
+
+const int mod = 1e9+7;
+int peopleAwareOfSecret(int n, int delay, int forget)
+{
+    vector<int> dp(n+1,0);
+    dp[0]=0;
+    dp[1]=1;
+    int count=0;
+    for(int i=2;i<=n;i++)
+    {
+        if(i-delay>0) count = (count+dp[i-delay])%mod;
+        if(i-forget>0) count = (count-dp[i-forget]+mod)%mod;
+        dp[i]=count%mod;     
+    }
+    int ans=0;
+    for(int i=n-forget+1;i<=n;i++) ans = (ans+dp[i])%mod;
+    return ans;
+}
+
+// recursion + memoization 
+
 const int mod = 1e9+7;
 
 int helper(int n, int delay, int forget,int value,vector<int>& dp,int day=1)
