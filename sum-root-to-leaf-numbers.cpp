94c132ec98ae5f13c9e5b870344495c15c54a346
Adil Mohamed M P <adilmohamedmp1@gmail.com>
2025-11-14 19:05:31 +0530

    Update sum-root-to-leaf-numbers.cpp

--- sum-root-to-leaf-numbers.cpp
+++ sum-root-to-leaf-numbers.cpp
@@ -1,20 +1,11 @@
-    int ans=0;
-    void sumNumbersHelper(TreeNode* root,int num=0)
+    int sumNumbers(TreeNode* root,int num=0)
     {
-        if(!root) return;
+        int ans=0;
+        if(!root) return 0;
         num*=10;
         num+=root->val;
-        sumNumbersHelper(root->left,num);
-        if(!root->left and !root->right)
-        {
-            ans+=num;
-            return;
-        }
-        sumNumbersHelper(root->right,num);
-
-    }
-
-    int sumNumbers(TreeNode* root,int num=0) {
-        sumNumbersHelper(root);
+        ans+=sumNumbers(root->left,num);
+        if(!root->left and !root->right) return num;
+        ans+=sumNumbers(root->right,num);
         return ans;
     }
